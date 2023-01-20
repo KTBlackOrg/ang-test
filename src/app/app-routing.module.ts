6c1d4ca1cb4794bb5d49aca6a41c4649bee137b4
ErikBurdett <erikaburdet@gmail.com>
2023-01-20 17:33:21 -0600

    Simple User Registration / Login / User Admin Panel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './gaurd/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserListingComponent } from './userlisting/userlisting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {component:LoginComponent,path:'login'},
+ {component:RegisterComponent,path:'register'},
+ {component:HomeComponent,path:'',canActivate:[AuthGuard]},
+ {component:UserListingComponent,path:'user',canActivate:[AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
